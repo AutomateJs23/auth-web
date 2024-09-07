c62be76fb03734b45043a7d8578d529de1540ef3
Jessadawut Sanomnao <jetsadawutlkb@gmail.com>
2024-09-08 02:20:13 +0700

    [INIT] init auth-web

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="icon" href="assets/favicon/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1>Hello Authentication Project!</h1>;
+}
