96ca30cec138f30795dfcf50a7de978efa1460d9
A.rismon <142784870+Arismonx@users.noreply.github.com>
2024-09-09 17:59:34 +0700

    [feat] create register page

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -17,8 +17,8 @@ const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
     console.log('Failed:', errorInfo);
 };
 
-export default function Login() {
-    return(
+export default function Register() {
+    return (
         <div className="bg-sky-100">
             <div className="flex min-h-screen items-center justify-center ">
                 <div className="flex flex-col md:flex-row w-full m-6 max-w-4xl bg-white shadow-md rounded-lg overflow-hidden">
@@ -27,7 +27,7 @@ export default function Login() {
                         <div className="flex justify-center mb-10 md:hidden">
                             <img className="w-28 h-auto" src="https://cdn.freelogovectors.net/wp-content/uploads/2023/09/next-js-logo-freelogovectors.net_.png" alt="Next.js Logo" />
                         </div>
-                        <h2 className="text-2xl text-center font-bold mb-6 text-gray-900">Login</h2>
+                        <h2 className="text-2xl text-center font-bold mb-6 text-gray-900">SignUp</h2>
                         <Form
                             name="login"
                             initialValues={{ remember: true }}
@@ -54,25 +54,47 @@ export default function Login() {
                                     className="border-gray-300 focus:ring-blue-500 focus:border-blue-500 rounded-md shadow-sm"
                                 />
                             </Form.Item>
+                            
+                            <Form.Item
+                                name="confirm"
+                                dependencies={['password']}
+                                hasFeedback
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please confirm your password!',
+                                    },
+                                    ({ getFieldValue }) => ({
+                                        validator(_, value) {
+                                            if (!value || getFieldValue('password') === value) {
+                                                return Promise.resolve();
+                                            }
+                                            return Promise.reject(new Error('The new password that you entered do not match!'));
+                                        },
+                                    }),
+                                ]}
+                            >
+                                <Input.Password 
+                                    placeholder="Confirm Password"
+                                    className="border-gray-300 focus:ring-blue-500 focus:border-blue-500 rounded-md shadow-sm"
+                                /> 
 
-                            <Form.Item name="remember" valuePropName="checked" className="flex items-center font-bold">
-                                <Checkbox>Remember me</Checkbox>
                             </Form.Item>
 
                             <Form.Item>
                                 <Button htmlType="submit" className="w-full font-bold bg-gradient-to-r from-cyan-500 to-blue-500 text-white">
-                                    Login
+                                    SignUp
                                 </Button>
                             </Form.Item>
                         </Form>
                         <div className='text-center py-2'>
                             <span className='text-sm'>
-                                Don't have an account? 
-                                <Link href="/register" className='text-cyan-600 font-medium hover:underline'> Sign up</Link>
+                                Already have an account?
+                                <Link href="/login" className='text-cyan-600 font-medium hover:underline'> Login</Link>
                             </span>
                         </div>
                     </div>
-                    
+
                     <div className="hidden md:flex md:w-1/2 bg-gradient-to-r from-cyan-200 to-blue-300  justify-center items-center">
                         <img className="md:w-1/2 w-28 h-auto mx-auto" src="https://cdn.freelogovectors.net/wp-content/uploads/2023/09/next-js-logo-freelogovectors.net_.png" alt="Next.js Logo" />
                     </div>
